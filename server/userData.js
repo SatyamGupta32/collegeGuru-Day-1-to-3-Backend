bdbb2c78c6a9b5235f656fcf4fdda57ff6b01d8d
Satyam Gupta <satyamg501@gmail.com>
2024-11-07 11:44:11 +0530

    first commit

--- server/userData.js
+++ server/userData.js
@@ -1,4 +1,12 @@
 module.exports = [
-    { name: 'Satyam Gupta', email: 'admin@gmail.com', password: 'adminpass', role: 'admin' },
-    { name: 'Sahil kumar', email: 'user@gmail.com', password: 'userpass', role: 'user' }
+    { name: 'Satyam Gupta', email: 'satyam.admin@gmail.com', password: 'adminpass1', role: 'admin' },
+    { name: 'Rahul Verma', email: 'rahul.admin@gmail.com', password: 'adminpass2', role: 'admin' },
+    { name: 'Anuj Singh', email: 'anuj.admin@gmail.com', password: 'adminpass3', role: 'admin' },
+    { name: 'Jagriti Sharma', email: 'jagriti.admin@gmail.com', password: 'adminpass4', role: 'admin' },
+    { name: 'Sahil Kumar', email: 'sahil.user@gmail.com', password: 'userpass1', role: 'user' },
+    { name: 'Neha Sharma', email: 'neha.user@gmail.com', password: 'userpass2', role: 'user' },
+    { name: 'Rohit Mehta', email: 'rohit.user@gmail.com', password: 'userpass3', role: 'user' },
+    { name: 'Anjali Singh', email: 'anjali.user@gmail.com', password: 'userpass4', role: 'user' },
+    { name: 'Vikram Patil', email: 'vikram.user@gmail.com', password: 'userpass5', role: 'user' },
+    { name: 'Pooja Nair', email: 'pooja.user@gmail.com', password: 'userpass6', role: 'user' }
 ];
