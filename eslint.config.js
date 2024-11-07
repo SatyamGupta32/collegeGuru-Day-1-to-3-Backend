dd83b6e1a394e1841085ecc58142d58d8242e2a2
Satyam Gupta <satyamg501@gmail.com>
2024-11-07 18:51:56 +0530

    first commit

--- eslint.config.js
+++ eslint.config.js
@@ -1,38 +1,22 @@
 import js from '@eslint/js'
 import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
   { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js}'],  // Only target JavaScript files (you can adjust this as needed)
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 2020,  // Use ECMAScript 2020
+      globals: globals.node,  // Use Node.js globals
       parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        ecmaVersion: 'latest',  // Latest ECMAScript version
+        sourceType: 'module',  // Allow ES module syntax
       },
     },
-    settings: { react: { version: '18.3' } },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
     rules: {
-      ...js.configs.recommended.rules,
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
-      'react/jsx-no-target-blank': 'off',
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      ...js.configs.recommended.rules,  // Use ESLint's recommended rules for JavaScript
+      'no-console': 'warn',  // Warn on console usage (you can adjust or remove this)
+      'no-unused-vars': 'warn',  // Warn on unused variables
     },
   },
 ]
