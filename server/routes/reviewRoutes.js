69f93f1b37236e1fbb1d0f69bf82b6b29ee23233
Satyam Gupta <satyamg501@gmail.com>
2024-11-09 23:26:45 +0530

    first commit

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -6,8 +6,8 @@ const errorHandler = require('../middleware/errorHandler');
 const router = express.Router();
 
 // Routes for managing reviews, with the roleCheck middleware applied
-router.post('/:collegeId/:userId/review', roleCheck, addReview);  
-router.get('/:collegeId/reviews', roleCheck, getReviews); 
+router.post('/:collegeId/:id/addReview', roleCheck, addReview);  
+router.get('/:collegeId/getReviews', roleCheck, getReviews); 
 
 // Error handler middleware (applies to all routes below it)
 router.use(errorHandler);
